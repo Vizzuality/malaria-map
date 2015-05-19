0fe3afbba1591aadaee7ce44e23b3d54c3c84c68
Adam Mulligan <adammulligan@fastmail.fm>
2015-05-19 14:27:05 +0100

    Show GEE layers on map

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,23 @@
-// create a map in the "map" div, set the view to a given place and zoom
-var map = L.map('map').setView([-26.5738, 31.5335], 9);
+var map = L.map('map', {
+  center: [-26.5738, 31.5335],
+  zoom: 9
+});
 
-// add a basemap tile layer
-L.tileLayer('http://a.tiles.mapbox.com/v3/darkit.lok07npe/{z}/{x}/{y}.png', {   
-}).addTo(map);
+var defaultBaseMap = L.tileLayer('http://a.tiles.mapbox.com/v3/darkit.lok07npe/{z}/{x}/{y}.png').addTo(map);
+var baseMaps = { "Road": defaultBaseMap };
 
-//add a GEE layer
-L.tileLayer('https://earthengine.googleapis.com/map/{mapId}/{z}/{x}/{y}?token={token}'
-).addTo(map);
+var layersConfig = [
+  // Fill me in
+  // {name: 'name', map_id: '', token: ''}
+];
+
+var malariaLayers = {};
+layersConfig.forEach(function(layerConfig, index) {
+  var layer = L.tileLayer('https://earthengine.googleapis.com/map/'+layerConfig.map_id+'/{z}/{x}/{y}?token='+layerConfig.token)
+  malariaLayers[layerConfig.name] = layer;
+
+  // Set first layer as default
+  if (index === 0) { layer.addTo(map); }
+});
+
+L.control.layers(baseMaps, malariaLayers).addTo(map);
