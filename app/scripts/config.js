62497f0a9b868e6a7d37babf5d512f38cde657f4
Adam Mulligan <adammulligan@fastmail.fm>
2015-05-21 11:59:33 +0100

    Roughly match Sergio's designs

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,12 +1,14 @@
-window.LAYERS_CONFIG = [
-  [ "f456e95c0223de4a9a20e8ae2b9c213a", "8e283e12a885e72837c1af2e509cd5b9", "Water Bodies Hansen" ],
-  [ "0e5909c039bb40eeb86ceaf32b77120c", "ee46e9467ee3eadcc9dff7a0fd432ff2", "Elevation" ],
-  [ "963887d5461bb6825d161d1967285412", "1c46f27404dab58ae2442ff8c6a20d91", "NDVI" ],
-  [ "dfa02ce3fff2490336e67e26a5e789b1", "a86e30293f72ed51afda6be5af1658ae", "NDWI" ],
-  [ "429f09700ce7ba037a1939b1d0d8730c", "1cb4471dae0f334a120fad567903f299", "LST" ],
-  [ "1b0d7e57e1cd08cf6b34ac2d53f6532d", "f7af60b71b3038ff60ee8d11583a6759", "Distance_Water Bodies" ],
-  [ "ac6c4be04252e1b4bbf3973c7a4e0cbc", "af83e31503a540dc3005822c2b0271cf", "Malaria Risk Map Resampled" ],
-  [ "942cf4464c6a8505f10179d9867bbfde", "991a6093118319639df8c10169178e18", "Malaria Risk Map" ],
-  [ "2ed2d0d19588881b4fe7e0117a7975c2", "8592bee798dc4ba671ee1426f6b8ae17", "Control cases" ],
-  [ "bdf71c91ceacd689aff2b5c275c1a8d4", "5bec3b78c630e53279b83370919f45f8", "Cases" ]
-];
+window.LAYERS_CONFIG = {
+  basemaps: [
+    [ "0e5909c039bb40eeb86ceaf32b77120c", "ee46e9467ee3eadcc9dff7a0fd432ff2", "Elevation" ],
+    [ "1b0d7e57e1cd08cf6b34ac2d53f6532d", "f7af60b71b3038ff60ee8d11583a6759", "Water Bodies Distance" ],
+    [ "963887d5461bb6825d161d1967285412", "1c46f27404dab58ae2442ff8c6a20d91", "NDVI" ],
+    [ "dfa02ce3fff2490336e67e26a5e789b1", "a86e30293f72ed51afda6be5af1658ae", "NDWI" ],
+    [ "429f09700ce7ba037a1939b1d0d8730c", "1cb4471dae0f334a120fad567903f299", "LST" ],
+  ],
+  overlays: [
+    [ "ac6c4be04252e1b4bbf3973c7a4e0cbc", "af83e31503a540dc3005822c2b0271cf", "Malaria Risk Map (Resampled)" ],
+    [ "f456e95c0223de4a9a20e8ae2b9c213a", "8e283e12a885e72837c1af2e509cd5b9", "Water Bodies Hansen" ],
+    [ "bdf71c91ceacd689aff2b5c275c1a8d4", "5bec3b78c630e53279b83370919f45f8", "Cases" ]
+  ]
+};
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,14 +2,39 @@
 
 var map = L.map('map', {
   center: [-26.5738, 31.5335],
-  zoom: 9
+  zoom: 9,
+  zoomControl: false
 });
 
-var defaultBaseMap = L.tileLayer('http://a.tiles.mapbox.com/v3/darkit.lok07npe/{z}/{x}/{y}.png').addTo(map);
-var baseMaps = { "Road": defaultBaseMap };
+new L.Control.Zoom({position: 'topright'}).addTo(map);
 
-var malariaLayers = {};
-LAYERS_CONFIG.forEach(function(layerConfig, index) {
+L.tileLayer('http://a.tiles.mapbox.com/v4/adammulligan.f826072c/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYWRhbW11bGxpZ2FuIiwiYSI6ImNiMDYxMzkwOGJjYTJjZjhmZmY3YmUyZTljMDZjZGNjIn0.E30Oy7L6hXBm2vtmQoWZJA').addTo(map);
+
+var cartocss = " #sz{ marker-fill-opacity: 0.9; marker-line-color: #FFF; marker-line-width: 1.5; marker-line-opacity: 1; marker-placement: point; marker-type: ellipse; marker-width: 10; marker-fill: #FF6600; marker-allow-overlap: true; }";
+var cartoLayer = cartodb.createLayer(map, {
+  user_name: 'simbiotica',
+  type: 'cartodb',
+  sublayers: [{
+    sql: "SELECT * FROM sz where featcode like 'PPL%'",
+    cartocss: cartocss
+  }]
+});
+
+map.on('overlayadd', function() {
+  cartoLayer.addTo(map);
+});
+
+var baseMaps = {};
+LAYERS_CONFIG.basemaps.forEach(function(layerConfig, index) {
+  var layer = L.tileLayer('https://earthengine.googleapis.com/map/'+layerConfig[0]+'/{z}/{x}/{y}?token='+layerConfig[1])
+  baseMaps[layerConfig[2]] = layer;
+
+  // Set first layer as default
+  if (index === 0) { layer.addTo(map); }
+});
+
+var malariaLayers = { };
+LAYERS_CONFIG.overlays.forEach(function(layerConfig, index) {
   var layer = L.tileLayer('https://earthengine.googleapis.com/map/'+layerConfig[0]+'/{z}/{x}/{y}?token='+layerConfig[1])
   malariaLayers[layerConfig[2]] = layer;
 
@@ -17,6 +42,6 @@ LAYERS_CONFIG.forEach(function(layerConfig, index) {
   if (index === 0) { layer.addTo(map); }
 });
 
-L.control.layers(baseMaps, malariaLayers).addTo(map);
+L.control.layers(baseMaps, malariaLayers, {position: 'topleft', collapsed: false}).addTo(map);
 
 })();
