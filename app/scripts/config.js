3680ed448fe226a8dac175cae41af41a91289ba5
Adam Mulligan <adammulligan@fastmail.fm>
2015-05-22 16:20:16 +0100

    Adds blank basemap, fixes styles for zoom

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,14 +1,14 @@
 window.LAYERS_CONFIG = {
   basemaps: [
-    [ "0e5909c039bb40eeb86ceaf32b77120c", "42e00888ab44ddd520f7ad451044265f", "Elevation" ],
-    [ "5bd229c53bb22a981b285dffb666681a", "efcce23844cf557b31c604f8f449dd1b", "Water Bodies Distance" ],
-    [ "963887d5461bb6825d161d1967285412", "553a167efe6c080b76ef3451f2edc7ca", "NDVI" ],
-    [ "dfa02ce3fff2490336e67e26a5e789b1", "75035dfb1b7449b35a3a1d616c25e844", "NDWI" ],
-    [ "f84b2e04893c6c9dba9fdbd6a75609a7", "33f9340ce688e2116adda2627cd7407f", "LST" ]
+    [ "49b92a7c45674159e1527e09bd37b4ff", "76afd5e40ce934e64280625fab016014", "Elevation" ],
+    [ "bbb2147e4d05fc77bb2bf1815b8cf23d", "d14d49abbd76e296085727f91cc22d8b", "Water Bodies Distance" ],
+    [ "963887d5461bb6825d161d1967285412", "7f221f14500fcd9b21f20afa6deef600", "NDVI" ],
+    [ "dfa02ce3fff2490336e67e26a5e789b1", "e05cd2ace540f93cfac4fce6cf29242a", "NDWI" ],
+    [ "f84b2e04893c6c9dba9fdbd6a75609a7", "7164d8f480c11ea14edea29250fdc8bb", "LST" ]
   ],
   overlays: [
-    [ "687ddada8cb8e3b96b510ccbfddaa086", "29841b5498c6d848088464a7a02d283a", "Malaria Risk (Resampled)" ],
-    [ "2a2e8bcc0c92b79ff609eff7a6b8c8ef", "a3d638f3c3cef9ac6bd5ea731b55a05b", "Malaria Risk" ],
-    [ "4192000e9fcb174328baa120d05a8ebc", "421242c1dc8635af26522a2b4c7cb8bf", "Water Bodies Hansen" ],
+    [ "687ddada8cb8e3b96b510ccbfddaa086", "0aed79f77c4ee98ef479851311c14a0a", "Malaria Risk (Resampled)" ],
+    [ "2a2e8bcc0c92b79ff609eff7a6b8c8ef", "63890edf04b61c3fbc783dd83681405c", "Malaria Risk" ],
+    [ "58241e4b99498e3dc4e770c734071d8c", "1b1a8b4fea0a7816879e8e75122e9c5d", "Water Bodies Hansen" ]
   ]
 };
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,13 +10,10 @@ new L.Control.Zoom({position: 'topright'}).addTo(map);
 
 L.tileLayer('http://a.tiles.mapbox.com/v4/adammulligan.f826072c/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYWRhbW11bGxpZ2FuIiwiYSI6ImNiMDYxMzkwOGJjYTJjZjhmZmY3YmUyZTljMDZjZGNjIn0.E30Oy7L6hXBm2vtmQoWZJA').addTo(map);
 
-var baseMaps = {};
+var baseMaps = { 'Basemap': L.tileLayer("").addTo(map) };
 LAYERS_CONFIG.basemaps.forEach(function(layerConfig, index) {
   var layer = L.tileLayer('https://earthengine.googleapis.com/map/'+layerConfig[0]+'/{z}/{x}/{y}?token='+layerConfig[1])
   baseMaps[layerConfig[2]] = layer;
-
-  // Set first layer as default
-  if (index === 0) { layer.addTo(map); }
 });
 
 var malariaLayers = { };
