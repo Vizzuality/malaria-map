a6ba2cbea6882ca94414e188c033a13cb65d052b
Adam Mulligan <adammulligan@fastmail.fm>
2015-05-22 16:47:46 +0100

    Paper overlay

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,24 +8,38 @@ var map = L.map('map', {
 
 new L.Control.Zoom({position: 'topright'}).addTo(map);
 
-L.tileLayer('http://a.tiles.mapbox.com/v4/adammulligan.f826072c/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYWRhbW11bGxpZ2FuIiwiYSI6ImNiMDYxMzkwOGJjYTJjZjhmZmY3YmUyZTljMDZjZGNjIn0.E30Oy7L6hXBm2vtmQoWZJA').addTo(map);
 
-var baseMaps = { 'Basemap': L.tileLayer("").addTo(map) };
-LAYERS_CONFIG.basemaps.forEach(function(layerConfig, index) {
-  var layer = L.tileLayer('https://earthengine.googleapis.com/map/'+layerConfig[0]+'/{z}/{x}/{y}?token='+layerConfig[1])
-  baseMaps[layerConfig[2]] = layer;
-});
+var baseLayers = {
+  'Road Map': L.tileLayer('http://a.tiles.mapbox.com/v4/adammulligan.f826072c/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYWRhbW11bGxpZ2FuIiwiYSI6ImNiMDYxMzkwOGJjYTJjZjhmZmY3YmUyZTljMDZjZGNjIn0.E30Oy7L6hXBm2vtmQoWZJA').addTo(map),
+  'Satellite': L.tileLayer('http://a.tiles.mapbox.com/v4/adammulligan.m8ei7b99/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYWRhbW11bGxpZ2FuIiwiYSI6ImNiMDYxMzkwOGJjYTJjZjhmZmY3YmUyZTljMDZjZGNjIn0.E30Oy7L6hXBm2vtmQoWZJA')
+};
 
-var malariaLayers = { };
+var groupedOverlays = {
+  "base": {},
+  "risk": {}
+}
+
 LAYERS_CONFIG.overlays.forEach(function(layerConfig, index) {
   var layer = L.tileLayer('https://earthengine.googleapis.com/map/'+layerConfig[0]+'/{z}/{x}/{y}?token='+layerConfig[1])
-  malariaLayers[layerConfig[2]] = layer;
+  groupedOverlays.risk[layerConfig[2]] = layer;
 
   // Set first layer as default
   if (index === 0) { layer.addTo(map); }
 });
 
-var layerControl = new L.Control.Layers(baseMaps, malariaLayers, {position: 'topleft', collapsed: false});
-layerControl.addTo(map);
+LAYERS_CONFIG.basemaps.forEach(function(layerConfig, index) {
+  var layer = L.tileLayer('https://earthengine.googleapis.com/map/'+layerConfig[0]+'/{z}/{x}/{y}?token='+layerConfig[1])
+  groupedOverlays.base[layerConfig[2]] = layer;
+});
+
+// Use the custom grouped layer control, not "L.control.layers"
+L.control.groupedLayers(baseLayers, groupedOverlays, {
+  exclusiveGroups: ["base"],
+  position: 'topleft',
+  collapsed: false
+}).addTo(map);
+
+//var layerControl = new L.Control.Layers(baseMaps, malariaLayers, {position: 'topleft', collapsed: false});
+//layerControl.addTo(map);
 
 })();
