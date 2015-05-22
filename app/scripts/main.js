e8570450bbcdea1b97813402a160c4950385cb33
Adam Mulligan <adammulligan@fastmail.fm>
2015-05-22 10:09:14 +0100

    Rearrange css

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,20 +10,6 @@ new L.Control.Zoom({position: 'topright'}).addTo(map);
 
 L.tileLayer('http://a.tiles.mapbox.com/v4/adammulligan.f826072c/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYWRhbW11bGxpZ2FuIiwiYSI6ImNiMDYxMzkwOGJjYTJjZjhmZmY3YmUyZTljMDZjZGNjIn0.E30Oy7L6hXBm2vtmQoWZJA').addTo(map);
 
-var cartocss = " #sz{ marker-fill-opacity: 0.9; marker-line-color: #FFF; marker-line-width: 1.5; marker-line-opacity: 1; marker-placement: point; marker-type: ellipse; marker-width: 10; marker-fill: #FF6600; marker-allow-overlap: true; }";
-var cartoLayer = cartodb.createLayer(map, {
-  user_name: 'simbiotica',
-  type: 'cartodb',
-  sublayers: [{
-    sql: "SELECT * FROM sz where featcode like 'PPL%'",
-    cartocss: cartocss
-  }]
-});
-
-map.on('overlayadd', function() {
-  cartoLayer.addTo(map);
-});
-
 var baseMaps = {};
 LAYERS_CONFIG.basemaps.forEach(function(layerConfig, index) {
   var layer = L.tileLayer('https://earthengine.googleapis.com/map/'+layerConfig[0]+'/{z}/{x}/{y}?token='+layerConfig[1])
