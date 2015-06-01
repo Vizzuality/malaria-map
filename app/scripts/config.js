fd401028521862b5f72703a75687c9cecb587fb0
Adam Mulligan <adammulligan@fastmail.fm>
2015-06-01 12:13:18 +0100

    Adds cartodb layers

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -17,8 +17,10 @@ window.LEGEND_CONFIG = {
   "LST": [24, 26, 28, 30, 32],
   "NDVI": [0.1, 0.2, 0.3, 0.5, 0.8],
   "NDWI": [-0.2, -0.1, 0, 0.4, 0.5],
-  "Elevation": [0, "0.1-0.2", "0.2-0.4", "0.4-0.6", "0.8-1"],
+  "Elevation": [200, 500, 1000, 1500, 2000],
   "Water Bodies Distance": [0, 20, 50, 100, 120],
   "Malaria Risk": [0.5, 1],
-  "Malaria Risk (Resampled)": [0.5, 1]
+  "Malaria Risk (Resampled)": [0.5, 1],
+  "Cities in Danger": [0, 0.5, 1],
+  "Case Data": ["Without", "With"],
 };
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,7 +9,7 @@ var map = L.map('map', {
 new L.Control.Zoom({position: 'topright'}).addTo(map);
 
 var baseLayers = {
-  'Road Map': L.tileLayer('http://a.tiles.mapbox.com/v4/adammulligan.f826072c/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYWRhbW11bGxpZ2FuIiwiYSI6ImNiMDYxMzkwOGJjYTJjZjhmZmY3YmUyZTljMDZjZGNjIn0.E30Oy7L6hXBm2vtmQoWZJA').addTo(map),
+  'Road Map': L.tileLayer('http://a.tiles.mapbox.com/v4/darkit.mb80h2ac/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGFya2l0IiwiYSI6IkhtblZxN2MifQ.4Se0dQvGFVxnnCrzPkoz3g').addTo(map),
   'Satellite': L.tileLayer('http://a.tiles.mapbox.com/v4/adammulligan.m8ei7b99/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYWRhbW11bGxpZ2FuIiwiYSI6ImNiMDYxMzkwOGJjYTJjZjhmZmY3YmUyZTljMDZjZGNjIn0.E30Oy7L6hXBm2vtmQoWZJA')
 };
 
@@ -18,23 +18,44 @@ var groupedOverlays = {
   "risk": {}
 }
 
-LAYERS_CONFIG.overlays.forEach(function(layerConfig, index) {
-  var layer = L.tileLayer('https://earthengine.googleapis.com/map/'+layerConfig[0]+'/{z}/{x}/{y}?token='+layerConfig[1])
-  groupedOverlays.risk[layerConfig[2]] = layer;
+var createRiskLayers = function() {
+  LAYERS_CONFIG.overlays.forEach(function(layerConfig, index) {
+    var layer = L.tileLayer('https://earthengine.googleapis.com/map/'+layerConfig[0]+'/{z}/{x}/{y}?token='+layerConfig[1])
+    groupedOverlays.risk[layerConfig[2]] = layer;
 
-  // Set first layer as default
-  if (index === 0) { layer.addTo(map); }
+    // Set first layer as default
+    if (index === 0) { layer.addTo(map); }
+  });
+};
+
+var createCartoLayer = function(name, url) {
+  return function(callback) {
+    cartodb.createLayer(map, url)
+      .on('done', function(layer) {
+        callback(null, {name: name, layer: layer});
+      });
+  };
+};
+
+async.parallel([
+  createCartoLayer("Case Data", "https://simbiotica.cartodb.com/api/v2/viz/deb87ad2-084a-11e5-aead-0e0c41326911/viz.json"),
+  createCartoLayer("Cities in Danger", "https://simbiotica.cartodb.com/api/v2/viz/81ede332-084a-11e5-aaa3-0e0c41326911/viz.json")
+], function(err, results) {
+  createRiskLayers();
+  results.forEach( function(result) {
+    groupedOverlays.risk[result.name] = result.layer;
+  });
+
+  LAYERS_CONFIG.basemaps.forEach(function(layerConfig, index) {
+    var layer = L.tileLayer('https://earthengine.googleapis.com/map/'+layerConfig[0]+'/{z}/{x}/{y}?token='+layerConfig[1])
+    groupedOverlays.base[layerConfig[2]] = layer;
+  });
+
+  L.control.customLayers(baseLayers, groupedOverlays, {
+    exclusiveGroups: ["base"],
+    position: 'topleft',
+    collapsed: false
+  }).addTo(map);
 });
-
-LAYERS_CONFIG.basemaps.forEach(function(layerConfig, index) {
-  var layer = L.tileLayer('https://earthengine.googleapis.com/map/'+layerConfig[0]+'/{z}/{x}/{y}?token='+layerConfig[1])
-  groupedOverlays.base[layerConfig[2]] = layer;
-});
-
-L.control.customLayers(baseLayers, groupedOverlays, {
-  exclusiveGroups: ["base"],
-  position: 'topleft',
-  collapsed: false
-}).addTo(map);
 
 })();
